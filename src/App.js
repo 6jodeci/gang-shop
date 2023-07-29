d8296125b233acb28b9c9af0e132adecfc129583
6jodeci <egor0263s@gmail.com>
2023-07-29 21:09:38 +0500

    add cart and items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,95 @@
+import React from "react"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },{
+          id: 2,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },{
+          id: 3,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 4,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 5,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 6,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 7,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 8,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100'
+        },
+        {
+          id: 9,
+          title: 'Кепка ISSAYA Thistle Logo Cord',
+          img:'cap.jpg',
+          desc: 'Кепка классической формы, декорированная вышивкой с лого.\n Регулируемый размер.\n Выполнена из плотного вельвета.',
+          category: 'cap',
+          price: '2.100',
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
-        <div>
-            <span className='logo'>Gang Shop</span>
-            <ul className='nav'>
-                <li>Магазин</li>
-                <li>Контакты</li>
-                <li>Про нас</li>
-            </ul>
-        </div>
-        <div className='presentation'></div>
+      <div>
+        <span className='logo'>Gang Shop</span>
+        <ul className='nav'>
+          <li>Магазин</li>
+          <li>Контакты</li>
+          <li>Про нас</li>
+        </ul>
+        <FaShoppingCart onClick={() =>setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+        {cartOpen && (
+          <div className='shop-cart'></div>
+        )}
+      </div>
+      <div className='presentation'></div>
     </header>
   )
 }
